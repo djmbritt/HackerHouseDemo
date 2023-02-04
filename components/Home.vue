97fcd6277174902f4e4913e71b754577fc3f6a66
djmbritt <david@effect.ai>
2023-02-05 00:22:52 +0300

    change logo color

--- components/Home.vue
+++ components/Home.vue
@@ -4,7 +4,7 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
       <a class="flex justify-center pt-8 sm:pt-0" href="https://nuxtjs.org" target="_blank">
-        <img src="https://nosana.io/img/Nosana_Logo_horizontal_color_white.svg" alt="Nosana">
+        <img src="https://nosana.io/img/Nosana_Logo_horizontal_color_black.svg" alt="Nosana">
       </a>
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <h2 class="text-2xl leading-7 font-semibold">
