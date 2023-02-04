f678125681e6681c647dd945b8c34a67daebc71d
djmbritt <david@effect.ai>
2023-02-05 00:29:31 +0300

    Update text

--- components/Home.vue
+++ components/Home.vue
@@ -10,7 +10,9 @@
         <h2 class="text-2xl leading-7 font-semibold">
           Demo day Istanbul! </h2>
           <br>
-          Welcome to our demo. We are excited to show you what we have been working on.
+          Welcome to our demo. 
+          <br>
+          We are excited to show you what we have been working on.
         <p class="mt-3 text-gray-600">
           Deployed from a Nosana Node.
           <br>
